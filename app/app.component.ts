c9c66f590516b19a9ea6e68a8f5ca5330dc6d5f2
firewall0118 <danielklein0118@gmail.com>
2016-09-27 16:17:31 +0200

    Add Sting of Array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero }}
+      </li>
+    </ul>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+  title: string;
+  myHero: string;
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  
+  constructor(){
+	  this.myHero = this.heroes[0];
+	  this.title = 'Tour of Heroes';
+  }
+}
